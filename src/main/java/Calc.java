45e8ce8af279d898d3b243d4e57745dbb3813e97
Vamshi <vamshi.yerrapati@gmail.com>
2018-04-19 12:23:49 +0530

    Initial commit of Ripple Maven Project

---
+++ src/main/java/Calc.java
@@ -0,0 +1,14 @@
+
+public class Calc {
+	public String Sample()
+	{
+		System.out.println("This is a sample application");
+		return "Sample";
+}
+	public int Sample1()
+	{
+		System.out.println("This is a sample application");
+		int result = 3+5;
+		return result;
+}
+}
---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class CalcTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
